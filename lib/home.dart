7db5ef8b7e9375da285584135ae790c4183ffa08
Tori Calasans <calasanstori@gmail.com>
2022-09-05 23:17:35 -0300

    feito a home contar. estudado alguns widgets

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int i = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      child: const Text("Tori Calasasns",
-          style: TextStyle(color: Colors.amber, fontSize: 8)),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("App bar da Tori"),
+          leading: const Icon(Icons.menu),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              i = i + 1;
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Center(
+          child: Container(
+              width: 300,
+              height: 300,
+              color: Colors.black,
+              child: Center(
+                  child: Text(i.toString(),
+                      style:
+                          const TextStyle(color: Colors.amber, fontSize: 40)))),
+        ));
   }
 }
+//Lista de Widget aprendidos
+//Container
+//Text
+//Scaffolder -> andaime, esqueleto
+//appbar 
+//icon
+//floatingActionbutton
+//center
